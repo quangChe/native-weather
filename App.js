5eab9f6ee270b1f436d2fc5b27be468f14683d71
quangChe <quang.n.che@gmail.com>
2019-02-21 23:23:53 -0800

    Feat: Working version of weather app
    Completed all basic functionality of the weather app. Connected the
    SearchInput component to the app to run a handler function that calls
    a weather API to provide accurate data. Also improved the look and
    responsiveness of the app by providing loading indication during
    async fetching from the weather API.

--- App.js
+++ App.js
@@ -6,16 +6,53 @@ import {
   Text, 
   KeyboardAvoidingView,
   Platform,
+  ActivityIndicator,
 } from 'react-native';
 
-import getImageForWeather from './utils/getImageForWeather';
+import getImageForWeather from './utils/img_gallery';
 
 import SearchInput from './components/SearchInput';
 
+import {fetchLocationId, fetchWeather} from './utils/api';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: '',
+      temperature: null,
+      weather: '',
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation('San Diego');
+  }
+
+  handleUpdateLocation = async newLocation => {
+    this.setState({loading: true});
+    const id = await fetchLocationId(newLocation);
+    fetchWeather(id)
+      .then(data => {
+        const wData = data;
+        wData.temperature = data.temperature.toFixed(0);
+        return this.setState({...wData, loading: false});
+      })
+      .catch(err => {
+        console.error(err);
+        return this.setState({
+          location: '',
+          temperature: null,
+          weather: '',
+          loading: true
+        });
+      })
+  }
+
   render() {
-    const location = 'San Francisco';
+    const {location, weather, temperature} = this.state;
 
     return (
       <KeyboardAvoidingView
@@ -23,23 +60,29 @@ export default class App extends React.Component {
         behavior="padding">
         
         <ImageBackground
-          source={getImageForWeather('Clear')}
+          fadeDuration="4s"
+          source={getImageForWeather(this.state.weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}>
 
           <View style={styles.detailsContainer}>
             <Text style={[styles.largeText, styles.textStyle, styles.special]}>
-              {location}
+              {this.state.loading ? '' : location}
             </Text>
             <Text style={[styles.smallText, styles.textStyle]}>
-              Light Cloud
+              {this.state.loading ? '' : weather}
             </Text>
-            <Text style={[styles.largeText, styles.textStyle]}>
-              54°
-            </Text>
+            {
+              this.state.loading 
+              ? <ActivityIndicator size="large" color="rgba(0, 0, 0, 0.8)"/>
+              : <Text style={[styles.largeText, styles.textStyle]}>
+                  {temperature}&deg;F
+                </Text>
+                }
+            
             <SearchInput 
-              location={location}
-              placeholder="Search any city"/>
+              placeholder="Search any city"
+              onSubmission={this.handleUpdateLocation}/>
           </View>            
         </ImageBackground>
       </KeyboardAvoidingView>
